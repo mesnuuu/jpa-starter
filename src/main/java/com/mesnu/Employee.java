a09882b9157fbdbd9a34d1454dd17bfe6fbf1eba
mesnuuu <mesnuuu@gmail.com>
2021-10-21 13:45:38 +0500

    Project Lambok

--- src/main/java/com/mesnu/Employee.java
+++ src/main/java/com/mesnu/Employee.java
@@ -4,6 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Data;
+
+@Data
 @Entity
 @Table(name = "EMPLOYEE_DATA")
  
@@ -13,30 +16,5 @@ public class Employee {
 	private int id;
 	private String name;
 
-	public Employee() {
-
-	}
-
-	public Employee(int id, String name) {
-		super();
-		this.id = id;
-		this.name = name;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
 
 }
