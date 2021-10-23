1529b95e51e9769d91019c2ebddc92ab9e8f7140
mesnuuu <mesnuuu@gmail.com>
2021-10-23 09:40:43 +0500

    Basic CRUD

---
+++ src/main/java/com/mesnu/AccessCard.java
@@ -0,0 +1,35 @@
+package com.mesnu;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class AccessCard {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	private Date issueDate;
+	
+	private boolean isActive;
+	
+	private String firmwareVersion;
+
+	public AccessCard(Date issueDate, boolean isActive, String firmwareVersion) {
+		super();
+		this.issueDate = issueDate;
+		this.isActive = isActive;
+		this.firmwareVersion = firmwareVersion;
+	}
+	
+	
+
+}
--- src/main/java/com/mesnu/JpaStarterMain.java
+++ src/main/java/com/mesnu/JpaStarterMain.java
@@ -1,6 +1,7 @@
 package com.mesnu;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -13,40 +14,53 @@ public class JpaStarterMain {
 
 		
 		
-//		Employee employee1 = new Employee(
-//											2, 
-//											"sam", 
-//											"1234", 
-//											new Date(),
-//											EmployeeType.CONTRACTOR );
-				
-//		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("myApp");
-//		
-//		EntityManager entityManager = entityManagerFactory.createEntityManager();
-//		
-//		EntityTransaction transaction = entityManager.getTransaction();
+		Employee emp1 = new Employee(1,"sam", "1234", new Date(), EmployeeType.CONTRACTOR );			
+		Employee emp2 = new Employee(2, "john", "4567", new Date(), EmployeeType.FULL_TIME );
 		
-//		transaction.begin();
-//			
-//		entityManager.persist(employee1);
-//
-//		transaction.commit();
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("myApp");	
+		EntityManager entityManager = entityManagerFactory.createEntityManager();	
+		EntityTransaction transaction = entityManager.getTransaction();
 		
-		
-		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("myApp");		
-		EntityManager entityManager = entityManagerFactory.createEntityManager();
-			
-		Employee emp = entityManager.find(Employee.class, 2);	
-		System.out.println(emp);
-		
-		emp.setName("John Smith");
-		
-		EntityTransaction transaction = entityManager.getTransaction();		
 		transaction.begin();	
-		entityManager.persist(emp);
-		entityManager.remove(emp);
-		transaction.commit();
 		
+		// CREATE
+		entityManager.persist(emp1);
+		entityManager.persist(emp2);
+		System.out.println("Creating Emplyoees ");
+		System.out.println(emp1);
+		System.out.println(emp2);
+		System.out.println("*******************");
+		
+		// READ
+		Employee empR = entityManager.find(Employee.class, 1);
+		System.out.println("Reading Emplyoee whose id=1");
+		System.out.println(empR);
+		System.out.println("*******************");
+		
+		// UPDATE
+		Employee empU = entityManager.find(Employee.class, 2);
+		System.out.println("Update Emplyoee whose id=2");
+		empU.setName("John Smith");
+		System.out.println(empU);
+		System.out.println("*******************");
+		
+		// DELETE
+		Employee empD = entityManager.find(Employee.class, 1);
+		System.out.println("Delete Emplyoee whose id=1");
+		entityManager.remove(empD);
+		System.out.println("*******************");
+		
+//		List<Employee> employees= entityManager.createQuery("from EMPLOYEE_DATA").getResultList();
+//		System.out.println(employees);
+		
+		AccessCard card1 = new AccessCard(new Date(), true, "1.0.0");		
+		AccessCard card2 = new AccessCard(new Date(), false, "1.2.0");
+		entityManager.persist(card1);
+		entityManager.persist(card2);
+		
+		
+		transaction.commit();
+			
 
 		
 	}
