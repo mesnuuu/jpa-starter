1b7a01f9a57a1212acb2854f7000d1cb2b053797
mesnuuu <mesnuuu@gmail.com>
2021-10-23 10:19:35 +0500

    Mapping Composition  ( One to One )
    # Mapping Composition  ( One to One )
    RULE OF THUMB
    1)	Each class of whole-part hierarchy is mapped to separate table.
    2)	To maintain whole-part relationship between
     	the whole ( composite class ) and
     		 the part ( component class )
    Primary key of composite class is inserted in the part classes as a foreign key.

--- src/main/java/com/mesnu/Employee.java
+++ src/main/java/com/mesnu/Employee.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -38,22 +39,29 @@ public class Employee {
 	
 	@Transient
 	private String debugString;
+	
+	@OneToOne
+	private AccessCard card;
 
 
-	public Employee(int id, String name, String nic, Date dob, EmployeeType type) {
+	public Employee(int id, String name, String nic, Date dob, EmployeeType type, AccessCard card) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.nic = nic;
 		this.dob = dob;
 		this.type = type;
-	}
+		this.card = card;
+	}	
 
 
 	@Override
 	public String toString() {
 		return "Employee [id=" + id + ", name=" + name + ", nic=" + nic + ", dob=" + dob + ", type=" + type + "]";
-	}	
+	}
+
+
+
 	
 	
 	
--- src/main/java/com/mesnu/JpaStarterMain.java
+++ src/main/java/com/mesnu/JpaStarterMain.java
@@ -11,54 +11,26 @@ import javax.persistence.Persistence;
 public class JpaStarterMain {
 	
 	public static void main(String[] args) {
+	
+		
+		AccessCard card1 = new AccessCard(new Date(), true, "1.0.0");		
+		AccessCard card2 = new AccessCard(new Date(), false, "1.2.0");
+		
+		Employee emp1 = new Employee(1,"sam", "1234", new Date(), EmployeeType.CONTRACTOR, card1 );			
+		Employee emp2 = new Employee(2, "john", "4567", new Date(), EmployeeType.FULL_TIME, card2 );
 
-		
-		
-		Employee emp1 = new Employee(1,"sam", "1234", new Date(), EmployeeType.CONTRACTOR );			
-		Employee emp2 = new Employee(2, "john", "4567", new Date(), EmployeeType.FULL_TIME );
 		
 		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("myApp");	
 		EntityManager entityManager = entityManagerFactory.createEntityManager();	
 		EntityTransaction transaction = entityManager.getTransaction();
 		
-		transaction.begin();	
+		transaction.begin();			
 		
-		// CREATE
 		entityManager.persist(emp1);
 		entityManager.persist(emp2);
-		System.out.println("Creating Emplyoees ");
-		System.out.println(emp1);
-		System.out.println(emp2);
-		System.out.println("*******************");
-		
-		// READ
-		Employee empR = entityManager.find(Employee.class, 1);
-		System.out.println("Reading Emplyoee whose id=1");
-		System.out.println(empR);
-		System.out.println("*******************");
-		
-		// UPDATE
-		Employee empU = entityManager.find(Employee.class, 2);
-		System.out.println("Update Emplyoee whose id=2");
-		empU.setName("John Smith");
-		System.out.println(empU);
-		System.out.println("*******************");
-		
-		// DELETE
-		Employee empD = entityManager.find(Employee.class, 1);
-		System.out.println("Delete Emplyoee whose id=1");
-		entityManager.remove(empD);
-		System.out.println("*******************");
-		
-//		List<Employee> employees= entityManager.createQuery("from EMPLOYEE_DATA").getResultList();
-//		System.out.println(employees);
-		
-		AccessCard card1 = new AccessCard(new Date(), true, "1.0.0");		
-		AccessCard card2 = new AccessCard(new Date(), false, "1.2.0");
 		entityManager.persist(card1);
 		entityManager.persist(card2);
-		
-		
+							
 		transaction.commit();
 			
 
